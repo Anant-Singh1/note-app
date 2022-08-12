a768d0c36848044c9327a8ac6ad1925782b9fa2f
Anant-Singh1 <97743499+Anant-Singh1@users.noreply.github.com>
2022-08-12 19:23:39 +0530

    Create basic structure of App

---
+++ src/component/Note.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { MdDeleteForever } from 'react-icons/md';
+const Note = () => {
+    return (
+        <div className="note">
+            <span className="">Hello...</span>
+            <div className="note-footer">
+                <small>13/01/22</small>
+                <MdDeleteForever className="delete-icon" size='1.3em' />
+            </div>
+        </div>
+    )
+}
+
+export default Note;
